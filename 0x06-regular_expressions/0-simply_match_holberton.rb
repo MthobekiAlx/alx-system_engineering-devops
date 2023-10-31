a5547023fc72a59a4ce5fb5c5cafdd5e52886853
Mthobeki <mthobekisikhosanag@gmail.com>
2023-10-31 14:05:47 +0200

    file

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,4 +1,24 @@
 #!/usr/bin/env ruby
-# Match "Holberton"
 
-puts ARGV[0].scan(/Holberton/).join
+# Check if there is a single argument provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit(1)
+end
+
+# Extract the argument from the command line
+input_string = ARGV[0]
+
+# Define the regular expression pattern to match "School"
+pattern = /School/
+
+# Use the regular expression to find matches in the input string
+matches = input_string.scan(pattern)
+
+# Output the matches
+if matches.empty?
+  puts ""
+else
+  puts matches.join
+end
+
